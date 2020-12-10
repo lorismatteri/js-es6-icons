d1731a85db0b841e12158b5ce62086ade546fa01
Loris Matteri <loris.matteri93@gmail.com>
2020-12-10 16:37:23 +0100

    Milestone 1
    inserito array con oggetti icone, fatte poi apparire in html

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -1,9 +1,71 @@
 $(document).ready(function () {
   // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
-
+  const icone = [
+    {
+      name: 'Cat',
+      prefix: 'fas',
+      type: 'fa-cat',
+      family: 'animals',
+    },
+    {
+      name: 'Dog',
+      prefix: 'fas',
+      type: 'fa-dog',
+      family: 'animals',
+    },
+    {
+      name: 'Crow',
+      prefix: 'fas',
+      type: 'fa-crow',
+      family: 'animals',
+    },
+    {
+      name: 'Dove',
+      prefix: 'fas',
+      type: 'fa-dove',
+      family: 'animals',
+    },
+    {
+      name: 'Dragon',
+      prefix: 'fas',
+      type: 'fa-dragon',
+      family: 'animals',
+    },
+    {
+      name: 'Horse',
+      prefix: 'fas',
+      type: 'fa-horse',
+      family: 'animals',
+    },
+    {
+      name: 'Hippo',
+      prefix: 'fas',
+      type: 'fa-hippo',
+      family: 'animals',
+    },
+    {
+      name: 'Fish',
+      prefix: 'fas',
+      type: 'fa-fish',
+      family: 'animals',
+    }
+  ];
+  console.log(icone);
   // Semezioniamo il container icons
-
+  const container = $('.icons');
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+  const print = () => {
+    icone.forEach(element => {
+      $(container).append(`
+      <div>
+      <i class="${element.prefix} ${element.type}"></i>
+      <div class="title">${element.name}</div>
+      </div>
+      `);
+    });
+  }
+
+  print();
 
 });
 
